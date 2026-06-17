f111bc7cf06ff574b17eb6bdef4c64cc3aa5fbf3
joshua2031 <joshua2031@snu.ac.kr>
2026-06-17 19:02:13 +0900

    hw1/FMPolicy

--- hw1/src/hw1_imitation/model.py
+++ hw1/src/hw1_imitation/model.py
@@ -65,7 +65,7 @@ class MSEPolicy(BasePolicy):
     # 'reshape' uses a view when possible, but copies the data if necessary, so it also works with non-contiguous tensors.
     # To use 'view' with a non-contiguous tensor:
     # x.contiguous().view(...)
-    
+
     def sample_actions(
         self,
         state: torch.Tensor,
@@ -91,13 +91,29 @@ class FlowMatchingPolicy(BasePolicy):
         hidden_dims: tuple[int, ...] = (128, 128),
     ) -> None:
         super().__init__(state_dim, action_dim, chunk_size)
+        self.input_dim = self.state_dim + self.chunk_size * self.action_dim + 1
+        self.model = nn.Sequential(
+        nn.Linear(self.input_dim, hidden_dims[0]),
+        nn.ReLU(),
+        nn.Linear(hidden_dims[0], hidden_dims[1]),
+        nn.ReLU(),
+        nn.Linear(hidden_dims[1], self.chunk_size * self.action_dim),
+        )
 
     def compute_loss(
         self,
+        # state: (B, state_dim)
         state: torch.Tensor,
+        # action_chunk: (B, chunk_dim, action_dim)
         action_chunk: torch.Tensor,
     ) -> torch.Tensor:
-        raise NotImplementedError
+        B = state.shape[0]
+        noise = torch.randn_like(action_chunk)
+        tau = torch.rand(B, 1, 1, device=action_chunk.device, dtype=action_chunk.dtype,)
+        interpolation = tau * action_chunk + (1 - tau) * noise
+        
+        input = torch.cat([state, interpolation.reshape(B, -1), tau.squeeze(-1)], dim=-1)
+        return torch.mean((self.model(input) - (action_chunk - noise).reshape(B, -1)) ** 2)
 
     def sample_actions(
         self,
@@ -105,8 +121,18 @@ class FlowMatchingPolicy(BasePolicy):
         *,
         num_steps: int = 10,
     ) -> torch.Tensor:
-        raise NotImplementedError
+        B = state.shape[0]
+        actions = torch.randn(B, self.chunk_size * self.action_dim, device=state.device, dtype=state.dtype,)
+        tau = torch.zeros(B, 1, device=state.device, dtype=state.dtype,)
+        dt = 1 / num_steps
 
+        for _ in range(num_steps):
+            input = torch.cat([state, actions, tau], dim=-1)
+            v = self.model(input)
+            actions = actions + v * dt
+            tau = tau + dt
+
+        return actions.reshape(-1, self.chunk_size, self.action_dim)
 
 PolicyType: TypeAlias = Literal["mse", "flow"]
 
